eaf8fc8b911021fc8cef9441451c32fdaa01201d
dawid.krawczyk <dawid.krawczyk@kernelpanic.com>
2020-01-16 09:31:22 +0100

    Fix- code smells

--- src/main/java/com/danix/code/smell/example001/CustomerReport.java
+++ src/main/java/com/danix/code/smell/example001/CustomerReport.java
@@ -1,16 +1,17 @@
 package com.danix.code.smell.example001;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import javax.annotation.Nonnull;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * @author  danix
+ * @author danix
  */
 public class CustomerReport {
 
     private final Customer customer;
     private final Account account;
+    private static final String ACCOUNT_IBAN = "Account: IBAN: ";
 
     public CustomerReport(@Nonnull final Customer customer, @Nonnull final Account account) {
         this.customer = checkNotNull(customer);
@@ -20,7 +21,7 @@ public class CustomerReport {
     public String printCustomerDaysOverdrawn() {
         String fullName = customer.getFullName();
 
-        String accountDescription = "Account: IBAN: " + account.getIban() + ", Days Overdrawn: "
+        String accountDescription = ACCOUNT_IBAN + account.getIban() + ", Days Overdrawn: "
                 + account.getDaysOverdrawn();
         return fullName + accountDescription;
     }
@@ -28,12 +29,12 @@ public class CustomerReport {
     public String printCustomerMoney() {
         String fullName = customer.getFullName();
         String accountDescription = "";
-        accountDescription += "Account: IBAN: " + account.getIban() + ", Money: " + account.getMoneyAmount();
+        accountDescription += ACCOUNT_IBAN + account.getIban() + ", Money: " + account.getMoneyAmount();
         return fullName + accountDescription;
     }
 
     public String printCustomerAccount() {
-        return "Account: IBAN: " + account.getIban() + ", Money: " + account.getMoneyAmount() + ", Account type: "
+        return ACCOUNT_IBAN + account.getIban() + ", Money: " + account.getMoneyAmount() + ", Account type: "
                 + account.getType();
     }
 
